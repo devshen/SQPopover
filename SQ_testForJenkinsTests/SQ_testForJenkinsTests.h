73d729203981016d82a62a02276a6eed917178f8
Quan Shen <shenquan1129@gmail.com>
2013-09-19 01:03:48 +0800

    Initial Commit

---
+++ SQ_testForJenkinsTests/SQ_testForJenkinsTests.h
@@ -0,0 +1,13 @@
+//
+//  SQ_testForJenkinsTests.h
+//  SQ_testForJenkinsTests
+//
+//  Created by Quan.Shen on 13-9-19.
+//  Copyright (c) 2013年 Quan.Shen. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SQ_testForJenkinsTests : SenTestCase
+
+@end
---
+++ SQ_testForJenkinsTests/SQ_testForJenkinsTests.m
@@ -0,0 +1,32 @@
+//
+//  SQ_testForJenkinsTests.m
+//  SQ_testForJenkinsTests
+//
+//  Created by Quan.Shen on 13-9-19.
+//  Copyright (c) 2013年 Quan.Shen. All rights reserved.
+//
+
+#import "SQ_testForJenkinsTests.h"
+
+@implementation SQ_testForJenkinsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SQ_testForJenkinsTests");
+}
+
+@end
